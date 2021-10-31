2b3990f897f5ed92b5ed40d38ac1369a2075e3f3
Ameer Sohail <ameersohail0@gmail.com>
2021-10-31 19:31:00 +0530

    export error resolved

--- spark/trainer.py
+++ spark/trainer.py
@@ -1,10 +1,13 @@
-import nltk
+# Import training libraries
+import pandas as pd
+import pickle
 
+import nltk
 nltk.download('stopwords')
 nltk.download('punkt')
+
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-
 
 import re
 import warnings
@@ -44,39 +47,3 @@ def process_text(text):
 
     text = " ".join(filtered_sentence)
     return text
-
-
-# training_data = pd.read_csv('train_data.csv', encoding='utf-8', sep='\t')
-training_data = pd.read_json('/app/data/news_new.json', encoding='utf-8')
-training_data = training_data.dropna()
-training_data['short_description'] = training_data['short_description'].apply(process_text)
-cat_list = [category_list.index(i) for i in training_data["category"]]
-training_data['flag'] = cat_list
-
-count_vect = CountVectorizer()
-X_train_counts = count_vect.fit_transform(training_data.short_description)
-tfidf_transformer = TfidfTransformer()
-X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-
-X_train, X_test, y_train, y_test = train_test_split(X_train_tfidf, training_data.flag,
-                                                    test_size=0.25, random_state=9)
-
-svm = svm.LinearSVC()
-
-model = CalibratedClassifierCV(svm)
-model.fit(X_train_tfidf, training_data.flag)
-
-pickle.dump(count_vect.vocabulary_, open("/app/models/count_vector.pkl", "wb"))
-pickle.dump(tfidf_transformer, open("/app/models/tfidf.pkl", "wb"))
-pickle.dump(model, open("/app/models/news_classifier.pkl", "wb"))
-
-predictions = model.predict(X_test)
-y_prob = model.predict_proba(X_test)
-
-probability = []
-
-for prediction, proba in zip(predictions, y_prob):
-    probability.append(proba[prediction])
-#
-# for prediction, prob in zip(predictions, probability):
-#     print(category_list[prediction] + " - " + str(prob*100))
