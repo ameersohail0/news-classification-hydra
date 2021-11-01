e90658e3f5a757bce4e42a16e63c261ac66d629c
Pavan Krishna <iampavankrishna@gmail.com>
2021-11-01 07:37:57 +0530

    minor changes

--- flask-streamer/news_streamer.py
+++ flask-streamer/news_streamer.py
@@ -1,15 +1,12 @@
 #! /usr/bin/python3                                                                                                      
 
-from kafka import KafkaProducer                                                                                         
-from random import randint                                                                                              
-from time import sleep, time                                                                                                 
+from kafka import KafkaProducer
+from time import time
 import sys
-import pandas as pd
 
 import os
 from dotenv import load_dotenv
 import requests
-import json
 
 # Sleep from Time library
 from time import sleep
@@ -29,14 +26,15 @@ url = "https://free-news.p.rapidapi.com/v1/search"
 
 headers = {
     'x-rapidapi-host': "free-news.p.rapidapi.com",
-    'x-rapidapi-key': os.getenv('API_KEY') # hide API key
+    'x-rapidapi-key': os.getenv('API_KEY')  # hide API key
     }
 
-def news_streamer(topic,timeout):
+
+def news_streamer(topic, timeout):
     now = time()
     page_no = 1
-    while (time() - now < 2 * timeout):
-        querystring = {"q":topic,"lang":"en","page":str(page_no),"page_size":"25"}
+    while time() - now < 2 * timeout:
+        querystring = {"q": topic, "lang": "en", "page": str(page_no), "page_size": "25"}
         response = requests.request("GET", url, headers=headers, params=querystring)
         try:
             for article in response.json()['articles']:
@@ -47,9 +45,9 @@ def news_streamer(topic,timeout):
                 except:
                     print("message can't be sent")                                                                      
                 sleep(1)
-        except :
+        except:
             print(f"max limit for topic {topic} reached")
         page_no += 1
 
 if __name__ == "__main__":
-    news_streamer(sys.argv[1],int(sys.argv[2]))
+    news_streamer(sys.argv[1], int(sys.argv[2]))
--- spark/utils.py
+++ spark/utils.py
@@ -1,5 +1,6 @@
 # Import libraries
 import os
+import re
 import subprocess
 
 import json
@@ -7,6 +8,8 @@ import pickle
 import time
 
 import pandas as pd
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 from sklearn import svm
 from sklearn.calibration import CalibratedClassifierCV
@@ -28,16 +31,16 @@ count_vect = CountVectorizer(vocabulary=pickle.load(open("models/count_vector.pk
 tfidf = pickle.load(open("models/tfidf.pkl", "rb"))
 
 category_list = ['POLITICS', "WELLNESS", 'ENTERTAINMENT',
-             "STYLE & BEAUTY", "TRAVEL", "PARENTING",
-             "FOOD & DRINK", "QUEER VOICES", "HEALTHY LIVING",
-             "BUSINESS", "COMEDY", "SPORTS", "HOME & LIVING",
-             "BLACK VOICES", "THE WORLDPOST", "WEDDINGS", "PARENTS",
-             "DIVORCE", "WOMEN", "IMPACT", "CRIME",
-             "MEDIA", "WEIRD NEWS", "WORLD NEWS", "TECH",
-             "GREEN", "TASTE", "RELIGION", "SCIENCE",
-             "MONEY", "STYLE", "ARTS & CULTURE", "ENVIRONMENT",
-             "WORLDPOST", "FIFTY", "GOOD NEWS", "LATINO VOICES",
-             "CULTURE & ARTS", "COLLEGE", "EDUCATION", "ARTS"]
+                 "STYLE & BEAUTY", "TRAVEL", "PARENTING",
+                 "FOOD & DRINK", "QUEER VOICES", "HEALTHY LIVING",
+                 "BUSINESS", "COMEDY", "SPORTS", "HOME & LIVING",
+                 "BLACK VOICES", "THE WORLDPOST", "WEDDINGS", "PARENTS",
+                 "DIVORCE", "WOMEN", "IMPACT", "CRIME",
+                 "MEDIA", "WEIRD NEWS", "WORLD NEWS", "TECH",
+                 "GREEN", "TASTE", "RELIGION", "SCIENCE",
+                 "MONEY", "STYLE", "ARTS & CULTURE", "ENVIRONMENT",
+                 "WORLDPOST", "FIFTY", "GOOD NEWS", "LATINO VOICES",
+                 "CULTURE & ARTS", "COLLEGE", "EDUCATION", "ARTS"]
 
 
 # function to load the model
@@ -46,36 +49,40 @@ def load_model():
     clf = pickle.load(open("models/news_classifier.pkl", "rb"))
     # load data if empty
     if articles.count() == 0:
-        with open("/app/data/news_new.json", encoding = 'utf-8') as f:
+        with open("/app/data/news_new.json", encoding='utf-8') as f:
             data = json.load(f)
             articles.insert_many(data)
 
 
 # function to predict the flower using the model
 def predict(query_data):
+    global count_vect, tfidf
     data = query_data
     x_new_counts = count_vect.transform(data.data)
     x_new_tfidf = tfidf.transform(x_new_counts)
     predictions = clf.predict(x_new_tfidf)
-    prob = clf.predict_proba(x_new_tfidf)
-    probs = [prob[prediction] for prediction in predictions]
+    predictions = [i for i in predictions]
+    probs = clf.predict_proba(x_new_tfidf)
+    probabilities = []
+    for prediction, prob in zip(predictions, probs):
+        probabilities.append(prob[prediction])
 
-    return predictions, probs
+    return predictions, probabilities
 
 
 def transformer(data):
     # listen to kafka and add data to pymongo
     time_delay = list(data.dict().values())[0]
-    proc1 = subprocess.Popen("spark-submit --jars spark-streaming-kafka-0-8-assembly_2.11-2.4.8.jar transformer.py", shell = True)
+    proc1 = subprocess.Popen("spark-submit --jars spark-streaming-kafka-0-8-assembly_2.11-2.4.8.jar transformer.py",
+                             shell=True)
     time.sleep(time_delay)
-    print ('killing proc1 = ', proc1.pid)
+    print('killing proc1 = ', proc1.pid)
     subprocess.Popen.kill(proc1)
     os.system("kill $(ps aux | grep 'pyspark' | awk '{print $2}')")
 
 
 # function to train and save the model as part of the feedback loop
 def train_model():
-
     # load the model
     global clf
     clf = pickle.load(open("models/news_classifier.pkl", "rb"))
@@ -83,7 +90,7 @@ def train_model():
     news_data = []
     for data in articles.find({}):
         news_data.append(data)
-    
+
     data = pd.DataFrame(news_data)
 
     if len(data) > 0:
@@ -97,12 +104,12 @@ def train_model():
         tfidf_transformer = TfidfTransformer()
         X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 
-        _,X_test,_,y_test = train_test_split(X_train_tfidf, data.flag,
-                                                            test_size=0.25, random_state=9)
+        _, X_test, _, y_test = train_test_split(X_train_tfidf, data.flag,
+                                                test_size=0.25, random_state=9)
 
         svm_model = svm.LinearSVC()  # Initializing the model instance
         clf = CalibratedClassifierCV(svm_model)
-        clf.fit(X_train_tfidf, data.flag) # Training the model
+        clf.fit(X_train_tfidf, data.flag)  # Training the model
         # clf.fit(X_train_tfidf, data.flag)  
 
         # Saving the model
@@ -117,7 +124,7 @@ def train_model():
             "message": "Training Successful",
             "accuracy": acc}
 
-    return {"message" : "Not enough new data to train"}
+    return {"message": "Not enough new data to train"}
 
 
 def process_text(text):
